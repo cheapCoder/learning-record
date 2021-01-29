845c3f1570a81d9056a5529cfb956002d350256b
liheng <1061555401@qq.com>
2021-01-29 18:02:17 +0800

    dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     filename: 'build.js',
     path: resolve(__dirname, "dist/"),
     publicPath: '',
+    assetModuleFilename: 'images/[name]-[hash][ext][query]',
   },
   module: {
     rules: [
@@ -30,13 +31,14 @@ module.exports = {
         loader: 'html-loader',
       },
       {
-        test: /\.(png|jpg|gif)$/i,
+        // test: /\.(png|jpg|gif)$/i,     // 打包图片
+        exclude: /\.(js|html|css)$/i,     // 打包其他资源，包括图片
         type: 'asset',
         parser: {
           dataUrlCondition: {
             maxSize: 8 * 1024
           }
-        }
+        },
       }
     ],
   },
@@ -44,5 +46,11 @@ module.exports = {
     alwaysWriteToDisk: true,
     template: resolve(__dirname, 'src/index.html'),
     filename: 'index.html',
-  })]
+  })],
+  devServer: {
+    contentBase: resolve(__dirname, '/dist'), //打包后监听的目录，
+    compress: true,
+    port: 9000,
+    open: true,  
+  }
 }
